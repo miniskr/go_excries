77c139b9197452679c33a091a1e6279e26a4d022
lyt <numb327@gmail.com>
2020-07-30 15:16:17 +0800

    sort map

--- chapter03/map/main.go
+++ chapter03/map/main.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	scene := make(map[string]int)
 
 	scene["route"] = 66
+	scene["brail"] = 4
+	scene["china"] = 86
 
-	fmt.Println(scene["route"])
+	// 无序
+	for k, v := range scene {
+		fmt.Println(k, v)
+	}
 
-	v := scene["route1"]
 
-	fmt.Println(v)
+	// 排序
+	// 声明一个切片保存map数据
+	var scene List []string
+
+	// 将map数据遍历复制到切片中
+	for k := range scene{
+		scene List = append(scene List, k)
+	}
+
+	// 对切片进行排序
+	sort.Strings(scene List)
+
+	// 输出
+	fmt.Println(scene List)
 }
