bd481e3f71d0ec823b8ab0cefcbde7119e411af9
lyt <numb327@gmail.com>
2020-07-30 13:25:04 +0800

    2.1.4

--- chapter02/main.go
+++ chapter02/main.go
@@ -2,35 +2,15 @@ package main
 
 import "fmt"
 
-func varableSwitch() {
-	// 多个变量同时赋值
-	// 1. 传统做法
-	// var a int = 100
-	// var b int = 200
-	// var temp int
-
-	// temp = a
-	// a = b
-	// b = temp
-
-	// 2. 高级做法 通过异或操作
-	// var a int = 100
-	// var b int = 200
-
-	// a = a ^ b
-	// b = b ^ a
-	// a = a ^ b
-
-	// fmt.Println(a, b)
-
-	//3. 简单做法
-	var a int = 100
-	var b int = 200
-
-	a, b = b, a
-	fmt.Println(a, b)
+func getData() (int, int) {
+	return 100, 200
 }
 
+// 匿名变量
 func main() {
-	varableSwitch()
+	a, _ := getData()
+
+	_, b := getData()
+
+	fmt.Println(a, b)
 }
